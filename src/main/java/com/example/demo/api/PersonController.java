cffae10bc1a2c32329d4da54e29f8803b1154b54
Mathieu Ibersien <mat98-76@live.fr>
2021-08-26 19:41:12 +0200

    api fonctionnelle

--- src/main/java/com/example/demo/api/PersonController.java
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,12 +44,13 @@ public class PersonController {
 		personService.addPerson(person);
 	}
 	
-	@DeleteMapping
-	public int deletePersonById(@PathVariable("id") UUID id) {
-		return personService.deletePerson(id);
+	@DeleteMapping(path = "{id}")
+	public void deletePersonById(@PathVariable("id") UUID id) {
+		personService.deletePerson(id);
 	}
 	
-	public int updatePersonById(@PathVariable("id") UUID id, @PathVariable("person") Person person) {
+	@PutMapping(path = "{id}")
+	public int updatePersonById(@PathVariable("id") UUID id, @RequestBody Person person) {
 		return personService.updatePerson(id, person);
 	}
 }
--- src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -47,9 +47,9 @@ public class FakePersonDataAccessService implements PersonDao {
 	public int updatePersonById(UUID id, Person person) {
 		return selectPersonById(id)
 				.map(p -> {
-					int indexOfPersonToDelete = DB.indexOf(person);
-					if(indexOfPersonToDelete >= 0) {
-						DB.set(indexOfPersonToDelete, person);
+					int indexOfPersonToUpdate = DB.indexOf(p);
+					if(indexOfPersonToUpdate >= 0) {
+						DB.set(indexOfPersonToUpdate, new Person(id, person.getName()));
 						return 1;
 					}
 					return 0;
